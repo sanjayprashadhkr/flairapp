8e10db569844e3004a42b6b843cea2bba219922c
Sanjayprashadhkr <sanjaykr1208@gmail.com>
2023-03-28 09:49:08 +0530

    Modified Hero.tsx

--- frontend/src/components/Home/Hero.tsx
+++ frontend/src/components/Home/Hero.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../../styles.css";
 import "./hero.css";
 
 export const Hero = () => {
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-// import { Hero } from "../components/Home/hero";
 import { useEffect } from "react";
+import { Hero } from "../components/Home/Hero";
 // import { updateUser } from "../reducers/userreducer";
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 // import { FeautureProduct } from "../components/Home/feautureProduct";
 export const Home = () => {
   // const dispatch = useDispatch();
 
   return (
     <div>
-      {/* <Hero /> */}
+      <Hero />
       {/* <Deals />
       <Arrivals /> */}
       {/* <FeautureProduct id1={0} id2={1} id3={2} id4={3} category="Deals" />
